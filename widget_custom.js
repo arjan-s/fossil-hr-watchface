701feb7b2e7d8c2422165b6a61650b8514bbb07a
Daniel Dakhno <dakhnod@gmail.com>
2021-11-09 16:13:47 +0100

    added jerky custom widgets

---
+++ widget_custom.js
@@ -0,0 +1,25 @@
+return {
+    node_name: '',
+    config: {
+        upper_text: '-',
+        lower_text: '-'
+    },
+    last_upper_text: '-',
+    last_lower_text: '-',
+    handler: function(event, response) {
+        if ((event.type === 'watch_face_update') || (event.type === 'display_data_updated')) {
+            this.draw(response)
+        }
+        if(event.type === 'node_config_update' && (this.config.upper_text != this.last_upper_text || this.config.lower_text != this.last_lower_text)){
+            this.draw(response)
+        }
+    },
+    draw: function(response) {
+        response.draw = {};
+        response.draw[this.node_name] = {
+            json_file: 'complication_layout',
+            dt: this.config.upper_text,  // 1st line
+            ci: this.config.lower_text,  // 2nd line
+        };
+    }
+};
